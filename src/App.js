cca99d61b8ad37cb47471fd0e798390471d1612c
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-06-22 16:58:34 -0400

    started to style header and form

--- src/App.js
+++ src/App.js
@@ -115,103 +115,130 @@ class App extends Component {
           <header>
             <h1>Pie me a Sliver</h1>
             <ol>
-              <li>Choose 3 emotions that you're feeling the most right now.  Tip: <em>You have to choose all 3!</em></li>
-              <li>Decide how much of each emotion you're feeling.  Tip: <em>These have to add up to 100%!</em></li>
+              <li>Choose 3 emotions that you're feeling the most right now.   <em>Note: You have to choose all 3!</em></li>
+              <li>Decide how much of each emotion you're feeling.  <em>Note: These have to add up to 100%!</em></li>
               <li>Press Submit and see your pie-chart come to life!</li>
             </ol>
           </header>
 
-          <main>
-            <form>
-              <h2>How are you feeling?</h2>
-              <fieldset>
-                <legend>Emotion 1</legend>
-                <select name="emotionAtype" id="" onChange={this.handleChange} value={this.state.emotionAtype}>
-                  <option value="">Select First Emotion</option>
-                  <option value="happy">Happy</option>
-                  <option value="elated">Elated</option>
-                  <option value="proud">Proud</option>
-                  <option value="amazed">Amazed</option>
-                  <option value="excited">Excited</option>
-                  <option value="determined">Determined</option>
-                  <option value="content">Content</option>
-                  <option value="hopeful">Hopeful</option>
-                  <option value="relieved">Relieved</option>
-                  <option value="joy">Joy</option>
-                </select>
-                <input 
-                  type="number" 
-                  name="emotionApercentage" 
-                  onChange={this.handleChange} 
-                  value={this.state.emotionApercentage}
-                  min="1"
-                  max="98"
-                />
-              </fieldset>
-              <fieldset>
-                <legend>Emotion 2</legend>
-                <select name="emotionBtype" id="" onChange={this.handleChange} value={this.state.emotionBtype}>
-                  <option value="">Select Second Emotion</option>
-                  <option value="insecure">Insecure</option>
-                  <option value="annoyed">Annoyed</option>
-                  <option value="anxious">Anxious</option>
-                  <option value="bored">Bored</option>
-                  <option value="frustrated">Frustrated</option>
-                  <option value="sad">Sad</option>
-                  <option value="jealous">Jealous</option>
-                  <option value="hurt">Hurt</option>
-                  <option value="disgusted">Disgusted</option>
-                  <option value="angry">Angry</option>
-                </select>
-                <input 
-                  type="number"
-                  name="emotionBpercentage"
-                  onChange={this.handleChange}
-                  value={this.state.emotionBpercentage} 
-                  min="1"
-                  max="98"
-                />
-              </fieldset>
-              <fieldset>
-                <legend>Emotion 3</legend>
-                <select name="emotionCtype" id="" onChange={this.handleChange} value={this.state.emotionCtype}>
-                  <option value="">Select Third Emotion</option>
-                  <option value="confused">Confused</option>
-                  <option value="hungry">Hungry</option>
-                  <option value="silly">Silly</option>
-                  <option value="peaceful">Peaceful</option>
-                  <option value="awed">Awed</option>
-                  <option value="awkward">Awkward</option>
-                  <option value="nostalgia">Nostalgia</option>
-                  <option value="triumphant">Triumphant</option>
-                  <option value="curious">Curious</option>
-                  <option value="entranced">Entranced</option>
-                </select>
-                <input
-                  type="number"
-                  name="emotionCpercentage"
-                  onChange={this.handleChange}
-                  value={this.state.emotionCpercentage}
-                  min="1"
-                  max="98"
-                />
-              </fieldset>
-              
-              
-              <div>
-                <p>Please Note: All the percentages you enter need to add up to 100%</p>
+          <div className="wrapper">
+          
+        
+            <main>
+              <form>
+
+                <h2>How are you feeling?</h2>
+
+                <div className="emotion-entry-container">
+
+                  <fieldset>
+                    <legend>Emotion 1</legend>
+                    <select 
+                      name="emotionAtype" 
+                      className="user-entry" 
+                      onChange={this.handleChange} 
+                      value={this.state.emotionAtype}
+                    >
+                      <option value="">Select First Emotion</option>
+                      <option value="happy">Happy</option>
+                      <option value="elated">Elated</option>
+                      <option value="proud">Proud</option>
+                      <option value="amazed">Amazed</option>
+                      <option value="excited">Excited</option>
+                      <option value="determined">Determined</option>
+                      <option value="content">Content</option>
+                      <option value="hopeful">Hopeful</option>
+                      <option value="relieved">Relieved</option>
+                      <option value="joy">Joy</option>
+                    </select>
+                    <input 
+                      type="number" 
+                      name="emotionApercentage" 
+                      className="user-entry"
+                      onChange={this.handleChange} 
+                      value={this.state.emotionApercentage}
+                      min="1"
+                      max="98"
+                    />
+                  </fieldset>
+
+                  <fieldset>
+                    <legend>Emotion 2</legend>
+                    <select 
+                      name="emotionBtype" 
+                      className="user-entry" 
+                      onChange={this.handleChange} 
+                      value={this.state.emotionBtype}
+                    >
+                      <option value="">Select Second Emotion</option>
+                      <option value="insecure">Insecure</option>
+                      <option value="annoyed">Annoyed</option>
+                      <option value="anxious">Anxious</option>
+                      <option value="bored">Bored</option>
+                      <option value="frustrated">Frustrated</option>
+                      <option value="sad">Sad</option>
+                      <option value="jealous">Jealous</option>
+                      <option value="hurt">Hurt</option>
+                      <option value="disgusted">Disgusted</option>
+                      <option value="angry">Angry</option>
+                    </select>
+                    <input 
+                      type="number"
+                      name="emotionBpercentage"
+                      className="user-entry"
+                      onChange={this.handleChange}
+                      value={this.state.emotionBpercentage} 
+                      min="1"
+                      max="98"
+                    />
+                  </fieldset>
+
+                  <fieldset>
+                    <legend>Emotion 3</legend>
+                    <select 
+                      name="emotionCtype" 
+                      className="user-entry" 
+                      onChange={this.handleChange} 
+                      value={this.state.emotionCtype}
+                    >
+                      <option value="">Select Third Emotion</option>
+                      <option value="confused">Confused</option>
+                      <option value="hungry">Hungry</option>
+                      <option value="silly">Silly</option>
+                      <option value="peaceful">Peaceful</option>
+                      <option value="awed">Awed</option>
+                      <option value="awkward">Awkward</option>
+                      <option value="nostalgia">Nostalgia</option>
+                      <option value="triumphant">Triumphant</option>
+                      <option value="curious">Curious</option>
+                      <option value="entranced">Entranced</option>
+                    </select>
+                    <input
+                      type="number"
+                      name="emotionCpercentage"
+                      className="user-entry"
+                      onChange={this.handleChange}
+                      value={this.state.emotionCpercentage}
+                      min="1"
+                      max="98"
+                    />
+                  </fieldset>
+
+                </div>
+
                 <p>{emotionPercentage} / 100%</p>
-              </div>
 
-              {(emotionPercentage === 100 && this.state.emotionAtype !== "" && this.state.emotionBtype !== "" && this.state.emotionCtype !== "") 
-                ? <button type="submit" onClick={this.addChartData}>Submit</button> 
-                : null}
-              
+                {(emotionPercentage === 100 && this.state.emotionAtype !== "" && this.state.emotionBtype !== "" && this.state.emotionCtype !== "") 
+                  ? <button type="submit" onClick={this.addChartData}>Submit</button> 
+                  : null}
+                
 
-            </form>
-          </main>
+              </form>
+            </main>
 
-        </div>
+          </div> {/* wrapper ends */}
+
+        </div> {/* form section ends */}
 
         
         <Display chartData={this.state.emotions} scrollDisplay={this.scrollDisplay}/>
