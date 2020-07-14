d285b2a1a16b82dffb46a9f2d3597c8048cc337e
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-07-14 14:55:08 -0400

    created colours branch; added colours object to state in Display component; made colours display in pie charts depending on user input

--- src/components/Display.js
+++ src/components/Display.js
@@ -6,6 +6,44 @@ import PieLegend from './PieLegend';
 
 class Display extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      colours: {
+        happy: "#FFD639",
+        elated: "#007CBE",
+        proud: "#2B59C3",
+        amazed: "#00AF54",
+        excited: "#0A81D1",
+        determined: "#FBAF00",
+        content: "#9F6BA0",
+        hopeful: "#62BBC1",
+        relieved: "#A1E8AF",
+        joy: "#EFBCD5",
+        insecure: "#55868C",
+        annoyed: "#DB7F67",
+        anxious: "#8B2635",
+        bored: "#F7B32B",
+        frustrated: "#F72C25",
+        sad: "#777DA7",
+        jealous: "#3A2449",
+        hurt: "#461220",
+        disgusted: "#5C573E",
+        angry: "#690500",
+        confused: "#934B00",
+        hungry: "#8AAA79",
+        silly: "#B3679B",
+        peaceful: "#B7D5D4",
+        awed: "#C4F7A1",
+        awkward: "#28666E",
+        nostalgic: "#71A9F7",
+        triumphant: "#FFD23F",
+        curious: "#F08080",
+        entranced: "#D1B1C8"
+      }
+    }
+  }
+
   render() {
 
     return (
@@ -32,17 +70,20 @@ class Display extends Component {
                         // Pie Chart rendered by passing in props below
                         data={[
                           {
-                            color: "#3F84E5",
+                            color: this.state.colours[emotionA.type],
+                            // color: "#3F84E5",
                             title: emotionA.type,
                             value: parseInt(emotionA.percentage)
                           },
                           {
-                            color: "#960200",
+                            color: this.state.colours[emotionB.type],
+                            // color: "#960200",
                             title: emotionB.type,
                             value: parseInt(emotionB.percentage)
                           },
                           {
-                            color: "#3F784C",
+                            color: this.state.colours[emotionC.type],
+                            // color: "#3F784C",
                             title: emotionC.type,
                             value: parseInt(emotionC.percentage)
                           },
@@ -52,7 +93,10 @@ class Display extends Component {
 
                   </div>
 
-                  <PieLegend emotionEntry={emotionEntry}/>
+                  <PieLegend 
+                    emotionEntry={emotionEntry}
+                    colours={this.state.colours}
+                  />
 
                   <button 
                     className="button pie-display-button"
